57d0c82827a6644efcfb7974da9f770ee572a8c8
Kamilunavo <acciento89@gmail.com>
2026-08-23 22:50:48 +0200

    production 0.11: add expedition mission state types

---
+++ Assets/Deadreach/Runtime/Missions/ExpeditionMissionTypes.cs
@@ -0,0 +1,20 @@
+namespace Kamilunavo.Deadreach.Missions
+{
+    public enum ExpeditionMissionType
+    {
+        Recovery = 0,
+        Purge = 1,
+        Holdout = 2,
+        Blacksite = 3
+    }
+
+    public enum ExpeditionMissionStage
+    {
+        Infiltrate = 0,
+        Activate = 1,
+        Eliminate = 2,
+        Recover = 3,
+        Hold = 4,
+        Complete = 5
+    }
+}
