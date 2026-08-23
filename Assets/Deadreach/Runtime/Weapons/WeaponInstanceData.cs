57d93fb65365c92079af2ee0e6246ff8a6ccad2d
Kamilunavo <acciento89@gmail.com>
2026-08-23 09:55:09 +0200

    feat: add serializable weapon instance and affix data

---
+++ Assets/Deadreach/Runtime/Weapons/WeaponInstanceData.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+namespace Kamilunavo.Deadreach.Weapons
+{
+    public enum WeaponAffixStat
+    {
+        DamagePercent = 0,
+        FireRatePercent = 1,
+        RangePercent = 2,
+        CritChancePercent = 3,
+        CritDamagePercent = 4
+    }
+
+    [Serializable]
+    public sealed class WeaponAffixRollData
+    {
+        public WeaponAffixStat stat;
+        public float value;
+    }
+
+    [Serializable]
+    public sealed class WeaponInstanceData
+    {
+        public string instanceId;
+        public string definitionId;
+        public string displayNameSnapshot;
+        public WeaponRarity rarity;
+        public int itemPower;
+        public List<WeaponAffixRollData> affixes = new();
+
+        public WeaponInstanceData Clone()
+        {
+            var copy = new WeaponInstanceData
+            {
+                instanceId = instanceId,
+                definitionId = definitionId,
+                displayNameSnapshot = displayNameSnapshot,
+                rarity = rarity,
+                itemPower = itemPower,
+                affixes = new List<WeaponAffixRollData>()
+            };
+
+            if (affixes != null)
+            {
+                foreach (var affix in affixes)
+                {
+                    copy.affixes.Add(new WeaponAffixRollData
+                    {
+                        stat = affix.stat,
+                        value = affix.value
+                    });
+                }
+            }
+
+            return copy;
+        }
+    }
+}
